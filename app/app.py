1166dcd12707cca973373f3b5daf076b158b6701
Carlos de França <carlos.franca@estudante.ufscar.br>
2024-11-28 23:56:45 -0300

    add predição

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, jsonify, request
+from .. import prediction
 
 app = Flask(__name__, template_folder='standalone', static_folder='standalone/static')
 
@@ -13,6 +14,20 @@ def static_files(path):
 @app.route('/')
 def iframe():
     return render_template("iframe_page.html")
+
+@app.route('/api/predict', methods='[POST]')
+def predict():
+    try:
+        data_json = request.get_json()
+        if data_json is None:
+            return jsonify({'error': 'erro'})
+        
+        molecule_smiles = data_json['smiles']
+        prediction = prediction.prediction(molecule_smiles)
+        
+        return jsonify({'prediction': prediction})
+    except:
+        return jsonify({'error': 'erro'})
 
 
 if __name__ == '__main__':
---
+++ prediction.py
@@ -0,0 +1,19 @@
+from lib.descriptor_gen import DescriptorGen
+import numpy as np
+import joblib
+
+model = joblib.load('models/oversampled_model.pkl')
+desc_gen = DescriptorGen()
+
+def prediction(molecule_smiles, model=model, desc_gen=desc_gen):
+    try:
+        desc = desc_gen.from_smiles(molecule_smiles)
+        desc = np.array(desc).reshape(1, -1)
+        pred = model.predict(desc)
+        return pred[0]
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 'error'
+    
+
+#print(prediction('CC1=CC2=C(C=C1)N(C3=C2CN(CC3)C)CCC4=CN=C(C=C4)C')) 
