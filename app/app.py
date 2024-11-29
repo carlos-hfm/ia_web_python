82d589ad5d3faa8e6691471e8220397c042f53b4
Carlos de França <carlos.franca@estudante.ufscar.br>
2024-11-28 23:21:42 -0300

    add pagina iframe

--- app/app.py
+++ app/app.py
@@ -2,13 +2,18 @@ from flask import Flask, send_from_directory, render_template
 
 app = Flask(__name__, template_folder='standalone', static_folder='standalone/static')
 
-@app.route('/')
-def home():
+@app.route('/ketcher')
+def ketcher():
     return render_template('index.html')
 
 @app.route('/<path:path>')
 def static_files(path):
     return send_from_directory('standalone', path)
+
+@app.route('/')
+def iframe():
+    return render_template("iframe_page.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
